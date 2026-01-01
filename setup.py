f054f52c1757552efdbf05d93cffb723db2d2620
Dries Peeters <dries.peeters96@gmail.com>
2026-01-01 13:31:46 +0100

    Version Bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='timetracker',
-    version='4.8.7',
+    version='4.8.8',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
