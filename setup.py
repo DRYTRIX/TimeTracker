4ba9d4feb966cf7e3462d7e1a9665b278077f9ec
Dries Peeters <dries.peeters96@gmail.com>
2026-01-05 19:33:03 +0100

    chore: bump version to 4.9.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='timetracker',
-    version='4.9.6',
+    version='4.9.7',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
