61be74680b430ab949984a430cd1d29dc2c8446b
Dries Peeters <dries.peeters96@gmail.com>
2026-02-28 17:30:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='timetracker',
-    version='4.20.6',
+    version='4.20.7',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
