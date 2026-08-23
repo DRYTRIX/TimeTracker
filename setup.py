b7a5a71fa3a4aa839dae1ec6013b86037f200929
Dries Peeters <dries.peeters96@gmail.com>
2026-08-23 19:02:55 +0200

    chore(release): bump version to 5.13.0 and update docs
    Syncs setup.py, desktop/package.json, mobile/pubspec.yaml, and
    browser-extension manifests to 5.13.0. Adds the corresponding
    CHANGELOG entry and README highlights for the release.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='timetracker',
-    version='5.12.0',
+    version='5.13.0',
     packages=find_packages(),
     include_package_data=True,
     package_data={
